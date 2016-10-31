094b8ae1ce2aa9c9fa83e41339a29a2511805420
Pavel <saymeales@gmail.com>
2016-11-01 02:11:40 +0300

    SwipeAction generic fix

--- recyclerviewadaptercreator/src/main/java/com/sayales/recyclerviewadaptercreator/SwipeAction.java
+++ recyclerviewadaptercreator/src/main/java/com/sayales/recyclerviewadaptercreator/SwipeAction.java
@@ -7,9 +7,9 @@ import com.sayales.recyclerviewadaptercreator.interfaces.Configured;
 /**
  * Created by Pavel on 30.10.2016.
  */
-public interface SwipeAction {
+public interface SwipeAction<T> {
     /*
     * Action witch would be called on item swipe
     * */
-    void action(int position, Configured data, RecyclerView.Adapter adapter);
+    void action(int position, Configured<T> data, RecyclerView.Adapter adapter);
 }
